e5339e80da08e88075bb681656996caceb853092
Muhammad Arsalan <50544594+arslangzr@users.noreply.github.com>
2023-03-14 11:49:15 +0500

    env file added

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,7 +1,18 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('crud_db', 'root', 'password', {
-    host: 'localhost',
+import dotenv from 'dotenv';
+dotenv.config();
+
+const dbConfig = {
+  host: process.env.db_host,
+  user: process.env.db_username,
+  password: process.env.db_password,
+  database: process.env.db_name
+};
+
+// Use the dbConfig object to connect to your database
+const db = new Sequelize(dbConfig.database,dbConfig.user,dbConfig.password, {
+    host: dbConfig.host,
     dialect: 'mysql'
 });
 
